0c72ac38952e71d4c2c256b66da3ee59e22aa870
okorieebube <okorieebube1@gmail.com>
2024-06-24 05:55:44 +0100

    setup: initial commit

---
+++ src/index.ts
@@ -0,0 +1,133 @@
+import { CLMM_PROGRAM_ID, DEVNET_PROGRAM_ID, OPEN_BOOK_PROGRAM, RAYMint, Raydium, TxVersion, USDCMint } from '@raydium-io/raydium-sdk-v2'
+import { Connection, Keypair, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import Decimal from 'decimal.js'
+import BN from 'bn.js'
+import bs58 from 'bs58'
+import dotenv from "dotenv"
+dotenv.config()
+
+const OWNER_PUB_KEY = process.env.OWNER_PUB_KEY;
+const OWNER_PRV_KEY = process.env.OWNER_PRV_KEY;
+const CLUSTER: 'devnet' | 'mainnet' = 'devnet';
+const CLUSTER_URL = process.env.RPC_URL ?? clusterApiUrl(CLUSTER);
+const connection = new Connection(CLUSTER_URL, "single");
+const txVersion = TxVersion.V0
+
+
+const initRaydium = async () => {
+    if (!OWNER_PRV_KEY) throw new Error('No prv key!');
+    const ownerKeyPair: Keypair = Keypair.fromSecretKey(bs58.decode(OWNER_PRV_KEY))
+
+    let raydium = await Raydium.load({
+        connection,
+        cluster: CLUSTER,
+        owner: ownerKeyPair,
+        disableFeatureCheck: true,
+        disableLoadToken: true,
+        blockhashCommitment: 'finalized',
+    })
+    // console.log({ xx: raydium.token })
+    return raydium;
+}
+// initRaydium()
+
+
+const createPool = async () => {
+    try {
+        if (!OWNER_PUB_KEY) throw new Error('No pub key!');
+
+        const raydium = await initRaydium()
+
+        // you can call sdk api to get mint info or paste mint info from api: https://api-v3.raydium.io/mint/list
+        // RAY
+        const mint1 = await raydium.token.getTokenInfo('CuKjdwbWLeTCVASfqzFCe4MavdhdHpx7Lx3oJfQhbGS5')
+        // USDT
+        const mint2 = await raydium.token.getTokenInfo('5UNpg5XxmTf1P6tLiCuN1zuz4y5Pim3hTvCt6wTHNzeo')
+        const clmmConfigs = await raydium.api.getClmmConfigs()
+        console.log({
+            programId: CLUSTER == "devnet" ? DEVNET_PROGRAM_ID.CLMM : CLMM_PROGRAM_ID,
+            owner: new PublicKey(OWNER_PUB_KEY),
+            mint1,
+            mint2,
+            ammConfig: { ...clmmConfigs[0], id: new PublicKey(clmmConfigs[0].id), fundOwner: '' },
+            initialPrice: new Decimal(1),
+            startTime: new BN(0),
+            txVersion
+        }); return;
+
+        /* const { execute } = await raydium.clmm.createPool({
+            programId: CLUSTER == "devnet" ? DEVNET_PROGRAM_ID.CLMM : CLMM_PROGRAM_ID,
+            owner: new PublicKey(OWNER_PUB_KEY),
+            mint1,
+            mint2,
+            ammConfig: { ...clmmConfigs[0], id: new PublicKey(clmmConfigs[0].id), fundOwner: '' },
+            initialPrice: new Decimal(1),
+            startTime: new BN(0),
+            txVersion,
+            // optional: set up priority fee here
+            // computeBudgetConfig: {
+            //   units: 600000,
+            //   microLamports: 100000000,
+            // },
+        })
+        // console.log({ xx: execute }); return;
+        const { txId } = await execute()
+        console.log('clmm pool created:', { txId }) */
+    } catch (error: any) {
+        console.error(error)
+        throw new Error(error)
+    }
+}
+createPool()
+
+export const createMarket = async () => {
+    try {
+        const raydium = await initRaydium()
+
+        const RAYMint = await new PublicKey('CuKjdwbWLeTCVASfqzFCe4MavdhdHpx7Lx3oJfQhbGS5')
+        const USDCMint = await new PublicKey('5UNpg5XxmTf1P6tLiCuN1zuz4y5Pim3hTvCt6wTHNzeo')
+
+        const { execute, extInfo, transactions } = await raydium.marketV2.create({
+            baseInfo: {
+                mint: RAYMint,
+                decimals: 6,
+            },
+            quoteInfo: {
+                mint: USDCMint,
+                decimals: 9,
+            },
+            lotSize: 1,
+            tickSize: 0.01,
+            dexProgramId: CLUSTER == "devnet" ? DEVNET_PROGRAM_ID.OPENBOOK_MARKET : OPEN_BOOK_PROGRAM,
+            // dexProgramId: DEVNET_PROGRAM_ID.OPENBOOK_MARKET, // devnet
+            txVersion,
+            // optional: set up priority fee here
+            // computeBudgetConfig: {
+            //   units: 600000,
+            //   microLamports: 100000000,
+            // },
+        })
+
+        console.log(
+            `create market total ${transactions.length} txs, market info: `,
+            Object.keys(extInfo.address).reduce(
+                (acc, cur) => ({
+                    ...acc,
+                    [cur]: extInfo.address[cur as keyof typeof extInfo.address].toBase58(),
+                }),
+                {}
+            )
+        )
+
+        const txIds = await execute({
+            // set sequentially to true means tx will be sent when previous one confirmed
+            sequentially: true,
+        })
+
+        console.log('create market txIds:', txIds)
+    } catch (error: any) {
+        console.error(error)
+        throw new Error(error)
+    }
+}
+// createMarket()
---
+++ src/spl-token-swap.ts
@@ -0,0 +1,14 @@
+
+
+/* 
+Initialization of a pool between two token types, which we'll call "A" and "B" for simplicity, requires the following accounts:
+
+empty pool state account
+pool authority
+token A account
+token B account
+pool token mint
+pool token fee account
+pool token recipient account
+token program
+ */
